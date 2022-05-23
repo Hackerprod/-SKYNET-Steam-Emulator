3d3afb3ec89ac5b62290f46cdfe5d274d8bb615a
Hackerprod <hackerprodlive@gmail.com>
2022-05-23 08:15:31 -0400

    Gamecoordinator implementation sample

---
+++ steam_api/Plugin/Dota2GameCoordinator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKYNET.Plugin
+{
+    public class Dota2GameCoordinator : IGameCoordinatorPlugin
+    {
+        private uint AppID;
+        public EventHandler<Dictionary<uint, byte[]>> IsMessageAvailable { get; set; }
+
+        public uint Initialize()
+        {
+            // TODO: Initialize all Game coordinator class
+            return AppID;
+        }
+
+        public void MessageFromGame(byte[] bytes)
+        {
+            // Process message from game
+            //uint MsgType = MsgUtil.GetGCMsg(new MemoryStream(bytes).ReadUInt32L());
+            //IPacketGCMsg packetGCMsg = MsgUtil.GetPacketGcMsg(MsgType, bytes);
+            // TODO: Process GC message
+        }
+
+        public void SendPacketToGame(uint msgType, byte[] packet)
+        {
+            Dictionary<uint, byte[]> message = new Dictionary<uint, byte[]>();
+            message.Add(msgType, packet);
+            IsMessageAvailable?.Invoke(this, message);
+        }
+
+        public void SendPacketToGame(Dictionary<uint, byte[]> messages)
+        {
+            IsMessageAvailable?.Invoke(this, messages);
+        }
+    }
+}
